0850a7edc5ef3ebeab0909db66f0d27be53b7c42
MinhThuIT03 <2351050175thu@ou.edu.vn>
2025-03-19 19:48:14 +0700

    up
    up

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelDes1 = new DicApp_Test.PanelDes();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(299, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dictionary";
+            // 
+            // panelDes1
+            // 
+            this.panelDes1.BackColor = System.Drawing.Color.White;
+            this.panelDes1.CornerRadius = 20;
+            this.panelDes1.Location = new System.Drawing.Point(48, 116);
+            this.panelDes1.Name = "panelDes1";
+            this.panelDes1.Size = new System.Drawing.Size(701, 54);
+            this.panelDes1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelDes1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private PanelDes panelDes1;
     }
 }
 
